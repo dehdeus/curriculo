57b590b2ebedcb82951f80050011f2592500d169
Deborah de Deus Tuba <deborahtuba@gmail.com>
2021-07-07 21:46:34 -0300

    Add últimas informações

--- src/components/experiencia.js
+++ src/components/experiencia.js
@@ -1,10 +1,79 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 import './experiencia.css'
 
 function Experiencia() {
     return (
-        <container id="expcontainer">
+        <container>
+            <section id="exptitulo">
+                <h2>EXPERIÊNCIA</h2>
+            </section>
+            <section className="section1">
+                <div className="bullets">
+                    <h4>LD Varejo e Representação</h4>
+                    <ol>Início: Abr/2015</ol>
+                    <ol>Término: Atual</ol>
+                </div>
+                <div className="bullets1">
+                    <li>Planejamento de Comunicação e Marketing para as Redes Sociais, Adward</li>
+                    <li>Atendimento ao Cliente e Fornecedor</li>
+                    <li>Gestão na plataforma Vtex – Estoque, categorias, Organização do Site</li>
+                    <li>Liderar projeto de implantação dos produtos em Marketplaces</li>
+                    <li>Atendimento ao Cliente e Fornecedor</li>
+                    <li>Gestão na plataforma Vtex – Estoque, categorias, Organização do Site</li>
+                    <li>Liderar projeto de implantação dos produtos em Marketplaces</li>
+                    <li>Gestão e implementação das campanhas em Marketplaces</li>
+                    <li>Interface com a plataforma do E-commerce</li>
+                    <li>Gestão e cadastro de produtos</li>
+                    <li>Gestão das contas de Marketplace</li>
+                    <li>Gestão de preços das contas de Marketplace</li>
+                    <li>Acompanhamento e gestão de indicadores e vendas da área</li>
+                    <li>Acompanhamento desempenho das campanhas de Marketplace</li>
+                    <li>Acompanhamento o fluxo de integrações necessárias para os canais</li>
+                    <li>Integração para Marketplace</li>
+                        <li>Parceiros de Marketplace, Pluggto, VTEX, Netshoes, B2W, ViaVarejo, Centauro, Amazon, Mercado Livre, Madeira Madeira, Carrefour, Centauro, Magazine Luiza</li>
+                </div>
+            </section>
+            <section className="section1">
+                <div className="bullets">
+                    <h4>Momentum Brasil</h4>
+                    <ol>Início: Set/2013</ol>
+                    <ol>Término: Abr/2015</ol>
+                </div>
+                <div className="bullets1">                   
+                    <li>Clientes: Bradesco, Nestlé, Nextel, GM, J&J, Nissan, Senac, Zurich, GoDaddy, Cultura Inglesa, Kimberley-Clark, Colgate, Sony, Editora Abril</li>
+                    <li>Produção e desenvolvimento de conteúdos para portais internos e externosda empresa</li>
+                    <li>Planejamento de ações voltadas para a comunicação corporativa</li>
+                    <li>Suporte à liderança com informações gerenciais para tomada de decisão</li>
+                    <li>Análise de indicadores e mercado</li>
+                    <li>Planejamento estratégico da área de comunicação</li>
+                    <li>Reporte direto ao Presidente</li>
+                    <li>Assessoria de imprensa</li>
+                    <li>Gestão de Mídias Sociais</li>
+                    <li>Planejamento e desenvolvimento de processos de integração de novos funcionários</li>
+                    <li>Estruturação da área de Comunicação com alinhamento global</li>
+                    <li>Agência global, exige alinhamento com diversos países, principalmente, Estados Unidos, Inglaterra, Chile e Argentina</li>
+                </div>
+            </section>
+            <section className="section1">
+                <div className="bullets">
+                    <h4>SKY Brasil</h4>
+                    <ol>Início: Jan/2011</ol>
+                    <ol>Término: Dez/2012</ol>
+                </div>
+                <div className="bullets1">
+                    <li>Estagiária</li>
+                    <li>Atendendo aproximadamente 2.500 funcionários</li>
+                    <li>Produção e desenvolvimento de conteúdos para os veículos internos da empresa</li>
+                    <li>Newsletter, conteúdo da TV Corporativa e e-mails</li>
+                    <li>Auxílio no planejamento de ações internas de endormarketing</li>
+                    <li>Gestão de indicadores da área</li>
+                    <li>Auxílio na organização de eventos internos</li>
+                </div>
+            </section>
+        </container>
+        
+        /* <container id="expcontainer">
             <section id="exp">
                 <h2>EXPERIÊNCIA</h2>
                     <div className="expbullets">
@@ -39,7 +108,7 @@ function Experiencia() {
             <section>
                 
             </section>
-        </container>
+        </container> */
     )
 }
 
